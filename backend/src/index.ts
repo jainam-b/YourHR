e4505102bd0ee31cf5edfbdce988f979821e376c
jainam-b <jainambagrecha16@gmail.com>
2024-08-27 11:27:21 +0530

    updates

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,9 +8,8 @@ const app = express();
 // Configure CORS
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://yourhr-jainam.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
+    origin: ["https://yourhr-jainam.vercel.app","http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"]
   })
 );
 
